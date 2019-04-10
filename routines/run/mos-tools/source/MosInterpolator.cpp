a07d717a85fa47c0af515a8ee201abc84a3cdc2f
Mikko Partio <mikko.partio@fmi.fi>
2019-04-10 10:05:05 +0000

    Same treatment to leadtime 135

--- routines/run/mos-tools/source/MosInterpolator.cpp
+++ routines/run/mos-tools/source/MosInterpolator.cpp
@@ -297,12 +297,12 @@ std::vector<datas> MosInterpolator::GetData(const MosInfo& mosInfo, const ParamL
 	}
 
 
-	// Fetching T-MEAN-K from previous forecast does not work for leadtime 141:
-	// leadtime 153 does not exist! In this case use data from leadtime 150.
+	// Fetching T-MEAN-K from previous forecast does not work for leadtime 135/141:
+	// leadtime 147/153 does not exist! In this case use data from leadtime 144/150.
 
-	if (paramName == "T-MEAN-K" && pl.originTimeAdjustment == -1 && step == 153)
+	if (paramName == "T-MEAN-K" && pl.originTimeAdjustment == -1 && (step == 147 || step == 153))
 	{
-		step = 150;
+		step -= 3;
 	}
 
 	auto prodInfo = itsRadonDB->GetProducerDefinition(producerId);
